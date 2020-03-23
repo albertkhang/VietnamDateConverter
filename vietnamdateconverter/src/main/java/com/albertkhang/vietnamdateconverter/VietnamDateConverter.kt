bfaaade596a0c28846069da129ddd4471490c5a1
Albert Khang <albertkhang2017@gmail.com>
2020-03-23 11:19:57 +0700

    make VietnamDateConverter singleton

--- vietnamdateconverter/src/main/java/com/albertkhang/vietnamdateconverter/VietnamDateConverter.kt
+++ vietnamdateconverter/src/main/java/com/albertkhang/vietnamdateconverter/VietnamDateConverter.kt
@@ -1,5 +1,17 @@
 package com.albertkhang.vietnamdateconverter
 
 class VietnamDateConverter {
-    
+    private lateinit var _instance: VietnamDateConverter
+
+    fun getInstance(): VietnamDateConverter {
+        if (_instance == null) {
+            synchronized(VietnamDateConverter::class) {
+                if (_instance == null) {
+                    _instance = VietnamDateConverter()
+                }
+            }
+        }
+
+        return _instance
+    }
 }
